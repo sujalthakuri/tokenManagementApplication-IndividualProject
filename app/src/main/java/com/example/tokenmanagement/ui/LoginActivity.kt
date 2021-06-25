ad2942b936a9a595a522fac1a3ddb0b8ccc95ec3
Sujal Singh Thakuri <sujalthakuribe@gmail.com>
2021-06-25 17:37:18 +0530

    Login, SignUp and Navigation Menu Added

--- app/src/main/java/com/example/tokenmanagement/ui/LoginActivity.kt
+++ app/src/main/java/com/example/tokenmanagement/ui/LoginActivity.kt
@@ -1,13 +1,12 @@
-package com.example.tokenmanagement
+package com.example.tokenmanagement.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tokenmanagement.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
+        setContentView(R.layout.activity_login)
     }
 }
--- app/src/main/java/com/example/tokenmanagement/ui/SignupActivity.kt
+++ app/src/main/java/com/example/tokenmanagement/ui/SignupActivity.kt
@@ -1,13 +1,12 @@
-package com.example.tokenmanagement
+package com.example.tokenmanagement.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tokenmanagement.R
 
-class MainActivity : AppCompatActivity() {
+class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
+        setContentView(R.layout.activity_signup)
     }
 }
